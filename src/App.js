e85a512ecdd2b0a93e603a237706124b5c7c33ce
saiaman9 <cratosash9@gmail.com>
2021-06-05 08:29:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      empDetails: [],
+      formDetails: {id:0, name: '', salary: ''},
+    };
+  }
+
+  componentDidMount = () => {
+    this.getEmpDetails();
+  }
+
+  getEmpDetails = () => {
+    axios.get('http://localhost:7000/employees').then(res=>{
+      this.setState({empDetails: res.data, formDetails: {id:0, name: '', salary: ''}});
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  textHandler = (e) => {
+    let updatedState = {...this.state};
+    updatedState.formDetails[e.target.id] = e.target.value;
+    this.setState(updatedState);
+  }
+
+  submitReq = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:7000/employees',this.state.formDetails).then(res=>{
+      console.log(res.data);
+      this.getEmpDetails();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+  render(){
+    const tableDataJSX = this.state.empDetails.map(e=>{
+      return(
+        <tr key={e.id}>
+          <th>{e.id}</th>
+          <th>{e.name}</th>
+          <th>{e.salary}</th>
+        </tr>
+      );
+    });
+    return (
+      <div className="App">
+        <table className={'table table-responsive'}>
+          <thead>
+            <tr>
+              <th>{'Emp Id'}</th>
+              <th>{'Name'}</th>
+              <th>{'Salary'}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableDataJSX}
+          </tbody>
+        </table>
+        <form onSubmit={(e)=>this.submitReq(e)}>
+              <input id='id' type='number' onChange={(e)=>this.textHandler(e)} value={this.state.formDetails.id} />
+              <input id='name' type='text' onChange={(e)=>this.textHandler(e)} value={this.state.formDetails.name} />
+              <input id='salary' type='number' onChange={(e)=>this.textHandler(e)} value={this.state.formDetails.salary} />
+              <button className={'btn btn-primary'} type={'submit'}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+
 }
 
 export default App;
